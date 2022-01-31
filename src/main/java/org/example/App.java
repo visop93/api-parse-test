20e36fba1c11108df4b0bbb6f2e0c0cce65471e8
visop93 <97960044+visop93@users.noreply.github.com>
2022-01-31 22:42:46 +0700

    will add logback and slf4j dependency's, app logger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        logger.info("Just a log message.");
+        logger.debug("Message for debug level.");
+
 //        CSVWorker.parseApiById();
 //        CSVWorker.writeItemsCSV(Items.ITEMS_MAP);
         Items.ITEMS_MAP = CSVWorker.readItemsCSV();
